bea841bac09861c383f00db7fce9cb4ed53d0fc1
Dan Cameron <dan@sproutapps.co>
2015-07-07 15:31:09 -0700

    Filter by auto billable invoices.

--- controllers/Auto_Billing_Admin.php
+++ controllers/Auto_Billing_Admin.php
@@ -20,10 +20,15 @@ class SI_Auto_Billing_Admin extends SI_Auto_Billing {
 
 		add_action( 'wp_ajax_' . self::AJAX_ACTION, array( get_class(), 'attempt_charge' ) );
 
-
 		// Invoice information option
 		add_action( 'doc_information_meta_box_date_row_last', array( __CLASS__, 'add_invoicing_auto_billing_option' ) );
 		add_action( 'si_save_line_items_meta_box', array( __CLASS__, 'save_auto_billing_selection' ) );
+
+		// Invoice admin table
+		add_action( 'restrict_manage_posts', array( __CLASS__, 'show_invoices_due' ) );
+		add_action( 'query_vars', array( __CLASS__, 'filter_admin_table_register_qvs' ) );
+
+		add_action( 'pre_get_posts', array( __CLASS__, 'filter_admin_table_results' ), 100 );
 	}
 
 	public static function attempt_charge() {
@@ -72,7 +77,7 @@ class SI_Auto_Billing_Admin extends SI_Auto_Billing {
 					$client_id = $invoice->get_client_id();
 					if ( $client_id ) {
 						if ( self::can_charge_client( $client_id ) ) {
-							printf( '<button class="payment_capture button button-small" data-client_id="%3$s" data-invoice_id="%2$s">%1$s</button>', sprintf( self::__( 'Attempt %s Payment' ), sa_get_formatted_money( $invoice->get_balance() ) ), $invoice_id, $client_id );
+							printf( '<button class="payment_capture button button-small" data-client_id="%3$s" data-invoice_id="%2$s">%1$s</button>', sprintf( self::__( 'Attempt %s Payment' ), sa_get_formatted_money( $invoice->get_balance() ) ), $id, $client_id );
 						}
 						else {
 							self::_e( 'Not setup or accepted' );
@@ -118,6 +123,60 @@ class SI_Auto_Billing_Admin extends SI_Auto_Billing {
 	}
 
 
+
+	////////////////////
+	// Invoices Table //
+	////////////////////
+
+	public static function show_invoices_due() {
+		global $typenow;
+		if ( SI_Invoice::POST_TYPE !== $typenow ) {
+			return;
+		}
+		$checked = ( isset( $_GET['only_auto_payments'] ) && '' !== $_GET['only_auto_payments'] ) ? true : false ;
+		printf( '<span class="table_filter_checkout"><label><input %s type="checkbox" name="only_auto_payments" value="1" id="auto_billing_option" />%s</label></span>', checked( $checked, true, false ), self::__( 'Only Auto Payments' ) );
+
+	}
+
+	public static function filter_admin_table_register_qvs( $query_vars ){
+		$query_vars[] = 'only_auto_payments';
+		return $query_vars;
+	}
+
+	public static function filter_admin_table_results( $query ) {
+		if ( is_admin() && SI_Invoice::POST_TYPE !== $query->query['post_type'] ) {
+			return;
+		}
+		$only_auto = $query->get( 'only_auto_payments' );
+		// Only if this query is set to filter out only auto payments
+		if ( ! empty( $only_auto ) ) {
+
+			global $wpdb;
+			// Make sure to accommodate the other post__in queries along with.
+			$posts_in = $query->get( 'post__in' );
+			$and_posts_in = '';
+			if ( ! empty( $posts_in ) ) {
+				$and_posts_in = sprintf( "AND $wpdb->posts.ID IN ( %s )", implode( ',', array_map( 'absint', $posts_in ) ) );
+			}
+			// get all the post ids that are auto billable.
+			$ids = $wpdb->get_col( $wpdb->prepare( "
+				SELECT ID
+				FROM $wpdb->posts
+				INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id ) WHERE 1=1 
+				AND ( ( $wpdb->postmeta.meta_key = '%s' AND CAST( $wpdb->postmeta.meta_value AS CHAR ) = true ) )
+				AND $wpdb->posts.post_type = %s
+				$and_posts_in
+				ORDER BY $wpdb->postmeta.meta_value DESC
+			", self::INVOICE_AUTOBILL_INVOICE, SI_Invoice::POST_TYPE ) );
+			
+			// If there are no results don't pass an empty array, otherwise WP will return all.
+			if ( empty( $ids ) ) {
+				$ids = array( 0 ); 
+			}
+			// Set to certain posts
+			$query->set( 'post__in', $ids );
+		}
+	}
 
 	/////////////////
 	// Meta boxes //
